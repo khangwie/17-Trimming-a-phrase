a1d5604e41b62e6236947522dbe09304114b62fd
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-04 22:40:34 +0200

    Add project files.

---
+++ 17 Trimming  a phrase/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myPhrase As String
+        myPhrase = InputBox("Enter your phrase")
+        Label1.Text = Trim(myPhrase)
+    End Sub
+End Class
